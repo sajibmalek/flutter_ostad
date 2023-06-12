35464af104309eea592297579281bba8c8368bb5
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2023-06-13 00:00:25 +0600

    update 2

--- home task/livetest3/lib/main.dart
+++ home task/livetest3/lib/main.dart
@@ -1,88 +1,66 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(app());
 }
 
 class app extends StatelessWidget {
-  const app({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "My Shopping List",
+      title: 'My Shopping List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: home(),
     );
   }
 }
 
 class home extends StatelessWidget {
-  const home({super.key});
-  snackMessage(message,context,c){
-    return ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message),
-          backgroundColor:  c,
-          //  textColor: Colors.red,
+  final List<String> shoppingItems = [
+    'Apples',
+    'Bananas',
+    'Oranges',
+    'Milk',
+    'Bread',
+  ];
 
-        )
-    );
-  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-appBar: AppBar(
-  title: Text("My Shopping List"),
-  centerTitle: true,
-  actions: [
-    Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: IconButton(onPressed: (){
-        snackMessage("Cart is empty",context,Colors.green);
-      }, icon:Icon(Icons.shopping_cart)),
-    )
-  ],
-),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text("Apples",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
 
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text("Bananas",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text("Bread",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text("Milk",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text("Eggs",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,),),
-            ),
+    snackMessage(message,context,c){
+      return ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(message),
+            backgroundColor:  c,
+            //  textColor: Colors.red,
+
+          )
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Shopping List'),
+          centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              snackMessage("Cart is empty",context,Colors.green);
+            },
           ),
         ],
+      ),
+      body: ListView.builder(
+        itemCount: shoppingItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text(shoppingItems[index]),
+          );
+        },
       ),
     );
   }
