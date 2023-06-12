c01ecd89908b88c1668d8a92cab2836bd19d0bbc
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2023-06-12 23:23:33 +0600

    Flutter Live test 3
    Requirements:
    
    ●     The page should have a Scaffold as the root widget.
    
    ●     Inside the Scaffold, create an AppBar with the title "My Shopping List".
    
    ●     Below the AppBar, create a ListView widget to display the list of shopping items.
    
    ●     Each item in the list should be represented by a ListTile widget.
    
    ●     Each ListTile should display an icon on the left, followed by the name of the shopping item.
    
    ●     The list should have at least 5 shopping items.
    
    ●     Add appropriate padding, spacing, and styling to make the app visually appealing.
    
    ●     Use the IconButton widget to add an icon button on the AppBar. When pressed, it should display a Snackbar with the message "Cart is empty".

--- home task/livetest3/lib/main.dart
+++ home task/livetest3/lib/main.dart
@@ -11,7 +11,79 @@ class app extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      title: "",
+      debugShowCheckedModeBanner: false,
+      title: "My Shopping List",
+      home: home(),
+    );
+  }
+}
+
+class home extends StatelessWidget {
+  const home({super.key});
+  snackMessage(message,context,c){
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(message),
+          backgroundColor:  c,
+          //  textColor: Colors.red,
+
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(
+  title: Text("My Shopping List"),
+  centerTitle: true,
+  actions: [
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: IconButton(onPressed: (){
+        snackMessage("Cart is empty",context,Colors.green);
+      }, icon:Icon(Icons.shopping_cart)),
     )
+  ],
+),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: Icon(Icons.shopping_bag),
+              title: Text("Apples",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
+
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: Icon(Icons.shopping_bag),
+              title: Text("Bananas",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: Icon(Icons.shopping_bag),
+              title: Text("Bread",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: Icon(Icons.shopping_bag),
+              title: Text("Milk",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              leading: Icon(Icons.shopping_bag),
+              title: Text("Eggs",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,),),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
