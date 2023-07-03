a29afbc96ca09abd5c4916d3e46d8bf92f168920
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2023-07-03 19:14:23 +0600

    module 8 live test demo  home

---
+++ home task/module8livetest/lib/HomeScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Module 8 live test"),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Text("Hello World"),
+          )
+        ],
+      ),
+    );
+  }
+}
