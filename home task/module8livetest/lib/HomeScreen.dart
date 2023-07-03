4dac1995e7fc0e385307f467cadabfa1a4ec3823
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2023-07-03 23:16:43 +0600

    final code
    ●     Create a screen to display a list of contacts.
    
    ●     The contact list should be populated with at least three contacts, including their names, email addresses, and phone numbers.
    
    ●     Each contact in the list should be represented as a ListTile widget.
    
    ●     Implement a tap gesture on each ListTile so that when a contact is tapped, a bottom sheet appears.
    
    ●     The bottom sheet should display the contact's details, including their name, email address, and phone number.
    
    ●     Ensure that the contact details are dynamically fetched from the tapped contact.
    
    ●     The bottom sheet should be dismissed when the user interacts with it.

--- home task/module8livetest/lib/HomeScreen.dart
+++ home task/module8livetest/lib/HomeScreen.dart
@@ -6,20 +6,91 @@ class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
 }
+class Contact {
+  final String name;
+  final String email;
+  final String phoneNumber;
+
+  Contact({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+  });
+}
+void _showContactDetails(BuildContext context, Contact contact) {
+  showModalBottomSheet<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'Name: ${contact.name}',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text('Email: ${contact.email}', style: TextStyle(fontSize: 16.0)),
+            SizedBox(height: 8.0),
+            Text('Phone: ${contact.phoneNumber}',
+                style: TextStyle(fontSize: 16.0)),
+            SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Close'),
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
 
 class _HomeState extends State<Home> {
+
+  final List<Contact> contacts = [
+    Contact(
+      name: "John Doe",
+      email: "johndoe@example.com",
+      phoneNumber: "123-456-7890",
+    ),
+    Contact(
+      name: "Jane Smith",
+      email: "janesmith@example.com",
+      phoneNumber: "987-654-3210",
+    ),
+    Contact(
+      name: "Alice Johnson",
+      email: "alicejohnson@example.com",
+      phoneNumber: "555-123-4567",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Module 8 live test"),
+        centerTitle: true,
+        title: Text("Contact List"),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Text("Hello World"),
-          )
-        ],
+      body: ListView.builder(
+        itemCount: contacts.length,
+        itemBuilder: (BuildContext context, index) {
+          return ListTile(
+            title: Text(contacts[index].name),
+            onTap: () {
+              _showContactDetails(context, contacts[index]);
+            },
+          );
+        },
       ),
     );
   }
